7add2cfa63391e98eccb2698b99ab6e0515b837c
سمیر صابر دادا <samirsabir788@gmail.com>
2024-07-02 06:57:55 +0400

    dist-update

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,28 @@
 module.exports = {
-  publicPath: ""
+  publicPath: "",
+  transpileDependencies: true,
+  pwa: {
+    name: "AfterSchool",
+    short_name: "AfterSchool",
+    workboxPluginMode: "GenerateSW",
+    skipWaiting: true,
+    workboxOptions: {
+      runtimeCaching: [
+        {
+          // Routing via a matchCallback function:
+          urlPattern: ({ request }) => request.destination === "image",
+          handler: "StaleWhileRevalidate"
+        },
+        {
+          urlPattern: ({ url }) =>
+            url.origin === "https://coursework-2-after-school.onrender.com" &&
+            url.pathname === "/collection/products",
+          handler: "StaleWhileRevalidate",
+          options: {
+            cacheName: "api-cache"
+          }
+        }
+      ]
+    }
+  }
 };
